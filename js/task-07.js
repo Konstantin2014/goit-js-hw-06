7a59276eb4270a9c670ad98251bfa5b71402cc14
Konstantin2014 <87135556+Konstantin2014@users.noreply.github.com>
2021-12-07 10:45:38 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const sizeControlEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
-
+textEl.style.fontSize = `${sizeControlEl.value}px`;
 const slider = () => {
   textEl.style.fontSize = `${sizeControlEl.value}px`;
 };
